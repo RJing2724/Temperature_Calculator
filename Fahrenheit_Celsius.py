ee5cf575b56c8fab15192b8873feb617ac47809e
RJing2724 <roy.zhuangjingbo@yahoo.com>
2022-04-13 03:15:26 -0400

    Click to start calculate

---
+++ Fahrenheit_Celsius.py
@@ -0,0 +1,7 @@
+# Fahrenheit_Celsius
+
+# c = Celsius, f = Fahrenheit
+c = input('Please enter temperature in degree celsius')
+c = float(c)
+f = c * (9/5) + 32
+print('The temperature is:', f, '`F')
